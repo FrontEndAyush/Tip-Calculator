ad3d33703ba39e9a0a24fe3c298c7a6b4901b710
ayush <iamrealglider@gmail.com>
2024-06-16 00:13:19 +0530

    made stater template

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <div className="w-full" >
+        <div className="mt-[100px]">
+          <h1 className="text-center text-[18px]  tracking-widest">SPLI</h1>
+          <h1 className="text-center text-[18px] tracking-widest">TTER</h1>
+        </div>
+
+        <div className="w-[800px] h-[350px] mt-5 bg-white flex mx-auto">
+          {/* first div  */}
+          <div></div>
+          {/* second div  */}
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
