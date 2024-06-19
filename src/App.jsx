30efb5b80ed25a12b9597619fa00e504ca11d5a2
ayush <iamrealglider@gmail.com>
2024-06-20 00:00:52 +0530

    tip calculators' desktop version done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
           </h1>
         </div>
 
-        <div className="w-[700px] p-9 h-[440px] rounded-2xl mt-14 font-bold bg-white flex mx-auto">
+        <div className="lg:w-[860px]  p-9 lg:h-[440px] h-full rounded-2xl mt-14 font-bold gap-7 bg-white flex flex-wrap lg:flex-nowrap mx-auto">
           {/* first main  div  */}
           <div>
             {/* first  */}
@@ -54,7 +54,11 @@ function App() {
                   <button className="border px-9  hover:bg-[#9FE8DF] hover:text-[#00474B] duration-300 py-1 text-[25px] bg-[#00474B] text-white rounded ">
                     30%
                   </button>
-                 <input type="number" className="w-[120px] text-[#00474B]  bg-[#F3F9F9] text-center text-[25px]" placeholder="Custom"  />
+                  <input
+                    type="number"
+                    className="w-[120px] text-[#00474B]  bg-[#F3F9F9] text-center text-[25px]"
+                    placeholder="Custom"
+                  />
                 </div>
 
                 <div></div>
@@ -78,6 +82,23 @@ function App() {
                 id="dollars"
               />
             </div>
+          </div>
+
+          <div className="w-[400px] relative bg-[#00474B] text-white px-6 h-[385px] ml-5 rounded-2xl">
+            <div className="flex mt-10 justify-between">
+              <h1>
+                Tip Amount <h1 className="text-[#66999D]">/ Person</h1>
+              </h1>
+              <h1 className="text-5xl text-[#1AC8AC]">$0.00</h1>
+            </div>
+            <div className="flex mt-10 justify-between">
+              <h1>
+                Tip Amount <h1 className="text-[#66999D]">/ Person</h1>
+              </h1>
+              <h1 className="text-5xl text-[#1AC8AC]">$0.00</h1>
+            </div>
+
+            <button className="absolute bottom-8 rounded py-2 w-[330px] hover:bg-[#9FE8DF] duration-300 mx-auto text-[#00474D] bg-[#26C2AD]">RESET</button>
           </div>
         </div>
       </div>
